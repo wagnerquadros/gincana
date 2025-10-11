93cf1d3f41a788839a1397b3efb43d46ade2992d
Wagner Oliveira de Quadros <106637044+wagnerquadros@users.noreply.github.com>
2025-10-11 16:22:21 -0300

    estrutura inicial

---
+++ backend/ginacana/src/test/java/com/gincana/GinacanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gincana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GinacanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
