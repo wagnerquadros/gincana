b73243ca44016cf8b5d63bf9c788c5da15847fee
Lucas de Lima Martinez <106637076+luscasmartinez@users.noreply.github.com>
2025-10-21 10:07:26 -0300

    Desenvolvido grande parte do sistema
    Durante o desenvolvimento do projeto, um merge realizado por outro desenvolvedor acabou quebrando o funcionamento do sistema. Para garantir a integridade do projeto e permitir a continuidade do desenvolvimento, optei por recriar e reorganizar o código com o auxílio da inteligência artificial.
    Conforme incentivo dos Gerentes

--- backend/src/adminSetup.js
+++
@@ -1,9 +0,0 @@
-// src/adminSetup.js
-const admin = require("firebase-admin");
-const serviceAccount = require("../serviceAccountKey.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-
-module.exports = admin;
