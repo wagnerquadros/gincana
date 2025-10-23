932317cae880ea22c5455c0e9c3d8f7d10d7a1aa
GianEsteves <115878514+GianEsteves@users.noreply.github.com>
2025-10-23 18:37:07 -0300

    fix(routes): correct route path typos in AppRoutes component

---
+++ frontend/src/routes/index.routes.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+import Initial from '../pages/Initial/Initial';
+import ActivityManagement from '../pages/ManagerPanel/Activity/activityManagement';
+import Dashboard from '../pages/ManagerPanel/Dashboard/dashboard';
+import Report from '../pages/ManagerPanel/Report/report';
+import Team from '../pages/ManagerPanel/Team/team';
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path='/' element={<Initial />} />
+      <Route path='/activity' element={<ActivityManagement />} />
+      <Route path='/dashboard' element={<Dashboard />} />
+      <Route paty='/report' element={<Report />} />
+      <Route paty='/team' element={<Team />} />
+    </Routes>
+  );
+}
+
+export default AppRoutes;
