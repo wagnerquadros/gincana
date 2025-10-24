7ef1baee538d5dcbc7537e811335e275612f67c5
GianEsteves <115878514+GianEsteves@users.noreply.github.com>
2025-10-24 17:42:45 -0300

    feat(Routes): add routes for DashboardTeam, ActivityTeam, History, and Responsible components

--- frontend/src/routes/index.routes.jsx
+++ frontend/src/routes/index.routes.jsx
@@ -4,15 +4,25 @@ import ActivityManagement from '../pages/ManagerPanel/Activity/activityManagemen
 import Dashboard from '../pages/ManagerPanel/Dashboard/dashboard';
 import Report from '../pages/ManagerPanel/Report/report';
 import Team from '../pages/ManagerPanel/Team/team';
-
+import DashboardTeam from '../pages/TeamPainel/DashboardTeam/dashboardTeam';
+import ActivityTeam from '../pages/TeamPainel/ActivityTeam/activityTeam';
+import History from '../pages/TeamPainel/History/history';
+import Responsible from '../pages/TeamPainel/Responsible/responsible';
 function AppRoutes() {
   return (
     <Routes>
+      {/* Rotas do painel do gestor */}
       <Route path='/' element={<Initial />} />
       <Route path='/activity' element={<ActivityManagement />} />
       <Route path='/dashboard' element={<Dashboard />} />
       <Route path='/report' element={<Report />} />
       <Route path='/team' element={<Team />} />
+
+      {/* Rotas do painel da equipe */}
+      <Route path='/dashboardTeam' element={<DashboardTeam />} />
+      <Route path='/activityTeam' element={<ActivityTeam />} />
+      <Route path='/history' element={<History />} />
+      <Route path='/responsible' element={<Responsible />} />
     </Routes>
   );
 }
